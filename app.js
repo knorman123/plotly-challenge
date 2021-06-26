b706ac1f744aad8322c15d8c298b47781adb5261
knorman123 <55822444+knorman123@users.noreply.github.com>
2021-06-26 01:41:10 -0700

    trying to fix errors

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 
  d3.json("samples.json").then((data) => {
     var metadata = data.metadata;
-    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == samples);
     var result = resultArray[0];
 
     console.log(result);
