c643572418df62d10f36ef331d5ff22d10e57608
knorman123 <55822444+knorman123@users.noreply.github.com>
2021-06-26 00:48:30 -0700

    seeing what it looks like in github pages

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -5,7 +5,7 @@
 
 //  When importing json, try using metadata
 
- d3.json("../../data/samples.json").then((data) => {
+ d3.json("samples.json").then((data) => {
     var metadata = data.metadata;
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
     var result = resultArray[0];
@@ -17,4 +17,4 @@
 
 
 //  Event Listener is different in this html, review id="selDataset" in index.html
- <select id="selDataset" onchange="optionChanged(this.value)"></select>
+//  <select id="selDataset" onchange="optionChanged(this.value)"></select>
