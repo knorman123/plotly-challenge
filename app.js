0b8e957ed1773e49482994120b3ead24faa55677
knorman123 <55822444+knorman123@users.noreply.github.com>
2021-06-27 22:05:29 -0700

    update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 
  d3.json("samples.json").then((data) => {
     var metadata = data.metadata;
-    var resultArray = metadata.filter(sampleObj => sampleObj.id == samples);
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == id);
     var result = resultArray[0];
 
     console.log(result);
