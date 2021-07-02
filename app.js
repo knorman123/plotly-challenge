4b7672b5377f5761e7e30f8bc8a367c3ecfaa6a4
knorman123 <55822444+knorman123@users.noreply.github.com>
2021-07-01 22:28:13 -0700

    added bar graph

--- app.js
+++ app.js
@@ -30,7 +30,38 @@ function data(sample) {
       Object.entries(result).forEach(([key, value]) => {
       panelBody.append("h6").text(`${key}: ${value} `);
       //console.log(`Key: ${key} and Value ${value}`);
+   
+   // Build plots
+   // set up filter for variables
+   var samples = data.samples;
+   var samplesArray = samples.filter(sampleObj => sampleObj.id == sample);
+   var arrayResult = samplesArray[0];
+   // set up variables for plots
+   var topSampleValues = arrayResult.sample_values.slice(0,10).reverse();
+   var topOtuIds = arrayResult.otu_ids.slice(0,10).map(otuid => "OTU" + otuid).reverse();
+   var topOtuLabels = arrayResult.otu_labels.slice(0,10);
+   
 
+   // Bar chart
+   var trace1 = {
+      x: topSampleValues,
+      y: topOtuIds,
+      type: 'bar',
+      orientation: 'h',
+      text: topOtuLabels,
+
+    };
+    var bar = [trace1];
+
+    var layout = {
+      title: "Top 10 OTUs in Sample",
+      yaxis: {
+         ticktext: topOtuLabels,
+      }
+      
+    };
+    
+    Plotly.newPlot("bar", bar, layout);
   })
  })
 }
@@ -40,8 +71,14 @@ function optionChanged(dropDownValue) {
    data(dropDownValue);
   
  };
+// Create function for plots
+// function buildPlots(dropDownValue) {
+   // get info from json
+//    d3.json("samples.json").then((data) => {
+//       var 
+//    })
+// }
 
-// Create drop down of sample ids
 
    
 // test the function
@@ -52,5 +89,4 @@ init();
 
 
 
-//  Event Listener is different in this html, review id="selDataset" in index.html
-//   <select id="selDataset" onchange="optionChanged(this.value)"></select>
+
