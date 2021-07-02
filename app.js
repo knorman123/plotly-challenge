4d442c74cc663723153d16bebae9f964fa6d50c6
knorman123 <55822444+knorman123@users.noreply.github.com>
2021-07-01 20:33:48 -0700

    populated drop down menu

--- app.js
+++ app.js
@@ -7,18 +7,28 @@
 function data(sample) {
  d3.json("samples.json").then((data) => {
     var metadata = data.metadata;
-    //var names = data.names;
+    var names = data.names;
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
     var result = resultArray[0];
 
      // console.log(result);
-  // Get the entries for each object in the array
-  Object.entries(result).forEach(([key, value]) => {
-   // Log the key and value
-   console.log(`Key: ${key} and Value ${value}`);
+  // Set panel body for demographics section
+  var panelBody = d3.select(".panel-body");
+      panelBody.html("");
+  // Get the entries for each object in the array, append to panel
+      Object.entries(result).forEach(([key, value]) => {
+      panelBody.append("h6").text(`${key}: ${value} `);
+      //console.log(`Key: ${key} and Value ${value}`);
+
+      var dropDown = d3.select("#selDataset");
+      names.forEach(name => dropDown.append("option").text(name).property("value", name))
+
   })
  })
 };
+
+// Create drop down of sample ids
+
    
 // test the function
 data('940')
