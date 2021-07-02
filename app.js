32e03a94fd9f56a4ea9eda0542b83d1e0f47ec4a
knorman123 <55822444+knorman123@users.noreply.github.com>
2021-07-01 20:03:25 -0700

    test the function to check iteration

--- app.js
+++ app.js
@@ -4,17 +4,29 @@
 
 
 //  When importing json, try using metadata
-
+function data(sample) {
  d3.json("samples.json").then((data) => {
     var metadata = data.metadata;
+    //var names = data.names;
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
     var result = resultArray[0];
 
-    console.log(result);
+     // console.log(result);
+  // Get the entries for each object in the array
+  Object.entries(result).forEach(([key, value]) => {
+   // Log the key and value
+   console.log(`Key: ${key} and Value ${value}`);
+  })
+ })
+};
+   
+// test the function
+data('940')
 
- });
+
+
 
 
 
 //  Event Listener is different in this html, review id="selDataset" in index.html
-  <select id="selDataset" onchange="optionChanged(this.value)"></select>
+//   <select id="selDataset" onchange="optionChanged(this.value)"></select>
